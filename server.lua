e9ae0107d6985f4427ac74f1583c092076c89485
Crew <113601819+CrewARGS@users.noreply.github.com>
2022-12-02 02:03:29 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-local market = createMarker(2131.88037, -1150.37109, 23.16541, "cylinder", 2)
+local market = createMarker(2131.88037, -1150.37109, 23.16541, "cylinder", 2) -- Maker yeridir. Hastanenin arkasındaki otopark
 
 aracTablo = {}
 
@@ -7,7 +7,7 @@ function aracVer(game)
         destroyElement(aracTablo[game])
     end
     local x, y, z = getElementPosition(game)
-    aracTablo[game] = createVehicle(436, 2147.51562, -1142.87805, 25.00839)
+    aracTablo[game] = createVehicle(436, 2147.51562, -1142.87805, 25.00839) --436 olan araç ID'sidir mta araç ıdleri kısmından değiştirebilirsiniz.
     warpPedIntoVehicle(game, aracTablo[game])
     outputChatBox("[CREW]:#ffffff Aracı başarılı bir şekilde aldınız.", game, 255, 0, 0, true)
 end
